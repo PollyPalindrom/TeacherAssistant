2ad20d96d617fd71b1a18222c24ebc1441bac6f3
Полина <polinaahrameiko@gmail.com>
2022-06-15 20:13:43 +0300

    rewrite entry fragment with compose

--- app/src/main/java/com/example/teacherassistant/ui/main/entryFragment/EntryFragment.kt
+++ app/src/main/java/com/example/teacherassistant/ui/main/entryFragment/EntryFragment.kt
@@ -4,18 +4,44 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.Image
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.res.painterResource
+import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.fragment.app.Fragment
-import com.example.teacherassistant.databinding.FragmentEntryBinding
+import com.example.teacherassistant.R
 
 class EntryFragment : Fragment() {
 
-    private lateinit var binding: FragmentEntryBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentEntryBinding.inflate(inflater, container, false)
-        return binding.root
+        return ComposeView(requireContext()).apply {
+            setContent {
+                EntryImage()
+            }
+        }
+    }
+
+    @Composable
+    fun EntryImage() {
+        ConstraintLayout {
+            val image = createRef()
+            Image(
+                painter = painterResource(R.drawable.ic_baseline_group_24),
+                contentDescription = "Entry Image",
+                modifier = Modifier.constrainAs(image) {
+                    top.linkTo(parent.top)
+                    bottom.linkTo(parent.bottom)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                }
+            )
+
+        }
     }
 
 }
